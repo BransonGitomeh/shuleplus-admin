8cbb1881a0d5e262d018e97e8484d20ff33def71
BransonGitomeh <sirbranson@outlook.com>
2020-11-04 12:00:03 +0300

    fix authflow for mobile screens

--- src/components/website_navbar.js
+++ src/components/website_navbar.js
@@ -99,6 +99,26 @@ class Navbar extends React.Component {
                 data-ktmenu-submenu-toggle="click"
                 aria-haspopup="true"
               >
+                <Link to="/auth" className="kt-menu__link">
+                  <span className="kt-menu__link-text">Sign In</span>
+                </Link>
+              </li>
+
+              <li
+                className="kt-menu__item  kt-menu__item--submenu kt-menu__item--rel"
+                data-ktmenu-submenu-toggle="click"
+                aria-haspopup="true"
+              >
+                <Link to="/register" className="kt-menu__link">
+                  <span className="kt-menu__link-text">Join Us</span>
+                </Link>
+              </li>
+
+              <li
+                className="kt-menu__item  kt-menu__item--submenu kt-menu__item--rel"
+                data-ktmenu-submenu-toggle="click"
+                aria-haspopup="true"
+              >
                 <Link to="/home" className="kt-menu__link">
                   <span className="kt-menu__link-text">Our Platform</span>
                 </Link>
@@ -107,25 +127,7 @@ class Navbar extends React.Component {
 
               </li>
 
-              {/* <li
-                className="kt-menu__item  kt-menu__item--submenu kt-menu__item--rel"
-                data-ktmenu-submenu-toggle="click"
-                aria-haspopup="true"
-              >
-                <Link to="/home" className="kt-menu__link">
-                  <span className="kt-menu__link-text">Partner with us</span>
-                </Link>
-              </li> */}
 
-              {/* <li
-                className="kt-menu__item  kt-menu__item--submenu kt-menu__item--rel"
-                data-ktmenu-submenu-toggle="click"
-                aria-haspopup="true"
-              >
-                <Link to="/home" className="kt-menu__link">
-                  <span className="kt-menu__link-text">About us</span>
-                </Link>
-              </li> */}
 
               <li
                 className="kt-menu__item  kt-menu__item--submenu kt-menu__item--rel"
@@ -141,57 +143,39 @@ class Navbar extends React.Component {
           </div>
         </div>
         {/* end: Header Menu */} {/* begin:: Header Topbar */}
-        {
-          !this.state.social_logged_in ?
-
-            <div
-              className="kt-header-menu-wrapper kt-grid__item"
-              id="kt_header_menu_wrapper"
-            >
-              <div
-                id="kt_header_menu"
-                className="kt-header-menu kt-header-menu-mobile "
+        <div
+          className="kt-header-menu-wrapper kt-grid__item"
+          id="kt_header_menu_wrapper"
+        >
+          <div
+            id="kt_header_menu"
+            className="kt-header-menu kt-header-menu-mobile "
+          >
+            <ul className="kt-menu__nav ">
+              <li
+                className="kt-menu__item  kt-menu__item--submenu kt-menu__item--rel"
+                data-ktmenu-submenu-toggle="click"
+                aria-haspopup="true"
               >
-                <ul className="kt-menu__nav ">
-                  <li
-                    className="kt-menu__item  kt-menu__item--submenu kt-menu__item--rel"
-                    data-ktmenu-submenu-toggle="click"
-                    aria-haspopup="true"
-                  >
-                    <Link to="/register" className="kt-menu__link">
-                      <span className="kt-menu__link-text">Sign up</span>
-                    </Link>
-                  </li>
-                </ul>
+                <Link to="/register" className="kt-menu__link">
+                  <span className="kt-menu__link-text">Sign up</span>
+                </Link>
+              </li>
+            </ul>
 
-                <ul className="kt-menu__nav ">
-                  <li
-                    className="kt-menu__item  kt-menu__item--submenu kt-menu__item--rel"
-                    data-ktmenu-submenu-toggle="click"
-                    aria-haspopup="true"
-                  >
-                    <Link to="/auth" className="kt-menu__link">
-                      <span className="kt-menu__link-text">Sign in</span>
-                    </Link>
-                  </li>
-                </ul>
-              </div>
-            </div>
-            : <div className="kt-header__topbar kt-grid__item kt-grid__item--fluid">
-              {/*begin: User bar */}
-              <div
-                className="kt-header__topbar-item kt-header__topbar-item--user"
-                id="kt_offcanvas_toolbar_profile_toggler_btn"
+            <ul className="kt-menu__nav ">
+              <li
+                className="kt-menu__item  kt-menu__item--submenu kt-menu__item--rel"
+                data-ktmenu-submenu-toggle="click"
+                aria-haspopup="true"
               >
-                <div className="kt-header__topbar-welcome">Hello,</div>
-                <div className="kt-header__topbar-username">{this.state.social_name}</div>
-                <div className="kt-header__topbar-wrapper">
-                  <img alt="Pic" src={this.state.social_profile_pic} />
-                </div>
-              </div>
-              {/*end: User bar */}
-            </div>
-        }
+                <Link to="/auth" className="kt-menu__link">
+                  <span className="kt-menu__link-text">Sign in</span>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
 
 
 
@@ -214,42 +198,6 @@ class Navbar extends React.Component {
         {/* {this.state.profileShowing ? <div className="kt-offcanvas-panel-overlay" /> : null} */}
 
         {/* fancy profile */}
-
-        <div id="kt_offcanvas_toolbar_profile" className="kt-offcanvas-panel">
-          <div className="kt-offcanvas-panel__head" kt-hidden-height={89} style={{}}>
-            <h3 className="kt-offcanvas-panel__title">
-              Profile
-          </h3>
-            <a href="#" className="kt-offcanvas-panel__close" id="kt_offcanvas_toolbar_profile_close"><i className="flaticon2-delete" /></a>
-          </div>
-          <div className="kt-offcanvas-panel__body kt-scroll ps ps--active-y" style={{ height: '500px', overflow: 'hidden' }}>
-            <div className="kt-user-card-v3 kt-margin-b-30">
-              <div className="kt-user-card-v3__avatar">
-                <img src="https://placeimg.com/140/140/any" alt="" />
-              </div>
-              <div className="kt-user-card-v3__detalis">
-                <a href="#" className="kt-user-card-v3__name">
-                  {/* {this.state.userData[Object.keys(this.state.userData)[0]].user} */}
-                </a>
-                <div className="kt-user-card-v3__desc">
-                  {/* {Object.keys(this.state.userData)[0]} */}
-                </div>
-                <div className="kt-user-card-v3__info">
-                  <a href="#" className="kt-user-card-v3__item">
-                    <i className="flaticon-email-black-circular-button kt-font-brand" />
-                    <span className="kt-user-card-v3__tag">gathoni@smartkids.com</span>
-                  </a>
-                  <a href="#" className="kt-user-card-v3__item">
-                    <i className="flaticon-twitter-logo-button kt-font-success" />
-                    <span className="kt-user-card-v3__tag">@gathoni</span>
-                  </a>
-                </div>
-
-                <button type="button" className="btn btn-brand btn-elevate btn-pill" >Sign out</button>
-              </div>
-            </div>
-            <div className="ps__rail-x" style={{ left: '0px', bottom: '0px' }}><div className="ps__thumb-x" tabIndex={0} style={{ left: '0px', width: '0px' }} /></div><div className="ps__rail-y" style={{ top: '0px', height: '500px', right: '0px' }}><div className="ps__thumb-y" tabIndex={0} style={{ top: '0px', height: '300px' }} /></div></div>
-        </div>
       </div>
     );
   }
