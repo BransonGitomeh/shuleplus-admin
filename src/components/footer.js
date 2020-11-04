56fffaa6b406519bbec6afbaadf0a9e933fb47ce
BransonGitomeh <sirbranson@outlook.com>
2020-11-04 16:12:33 +0300

    fix busses table issues

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,46 +2,46 @@ import React from "react";
 
 class Footer extends React.Component {
   render() {
-    return null;
-    // return (
-    //   <div className="kt-footer kt-grid__item" id="kt_footer">
-    //     <div className="kt-container ">
-    //       <div className="kt-footer__copyright">
-    //         2019&nbsp;©&nbsp;
-    //         <a
-    //           href="#"
-    //           target="_blank"
-    //           className="kt-link"
-    //         >
-    //           SmartKids Inc
-    //         </a>
-    //       </div>
-    //       <div className="kt-footer__menu">
-    //         <a
-    //            href="#"
-    //           target="_blank"
-    //           className="kt-link"
-    //         >
-    //           About
-    //         </a>
-    //         <a
-    //            href="#"
-    //           target="_blank"
-    //           className="kt-link"
-    //         >
-    //           Team
-    //         </a>
-    //         <a
-    //            href="#"
-    //           target="_blank"
-    //           className="kt-link"
-    //         >
-    //           Contact
-    //         </a>
-    //       </div>
-    //     </div>
-    //   </div>
-    // );
+    // return null;
+    return (
+      <div className="kt-footer kt-grid__item" id="kt_footer">
+        <div className="kt-container ">
+          <div className="kt-footer__copyright">
+            {new Date().getFullYear()} &nbsp;©&nbsp;
+            <a
+              href="#"
+              target="_blank"
+              className="kt-link"
+            >
+              ShulePlus
+            </a>
+          </div>
+          <div className="kt-footer__menu">
+            {/* <a
+               href="#"
+              target="_blank"
+              className="kt-link"
+            >
+              About
+            </a>
+            <a
+               href="#"
+              target="_blank"
+              className="kt-link"
+            >
+              Team
+            </a>
+            <a
+               href="#"
+              target="_blank"
+              className="kt-link"
+            >
+              Contact
+            </a> */}
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
